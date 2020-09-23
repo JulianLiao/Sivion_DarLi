8e746a054cbf71b38d84e42edaf4e6a52664a4c0
Meng Liao <meng.liao@perceptin.io>
2020-09-23 10:38:49 +0800

    写了一小部分代码。

--- project1/src/pi_vio.cpp
+++ project1/src/pi_vio.cpp
@@ -1,8 +1,20 @@
 
+#include <Eigen/Geometry>
+
 // prototype: void InitTwe(const Eigen::Isometry3d& T_b_e);
+/**
+ *  T_gpsantenna_e: 从天线坐标系到ENU坐标系
+ *  T_b_gpsantenna: 从车体坐标系到天线坐标系
+ *  T_b_e: 从车体坐标系到ENU坐标系
+ */
 void InitTwe(const Eigen::Isometry3d& T_gpsantenna_e) {
     Eigen::Isometry3d T_b_e;
     T_b_e = T_gpsantenna_e * T_b_gpsantenna;
+    Eigen::Vector4d Q_b_w_;
+    Eigen::Vector3d t_b_w_;
+
+
+    is_inited_T_w_e = true;
 
 }
 
